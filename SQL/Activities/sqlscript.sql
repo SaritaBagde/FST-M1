847e694c3f7ecf124a132ac9ba8f74f52526d6c2
SaritaBagde <160853201+SaritaBagde@users.noreply.github.com>
2024-05-10 09:52:24 +0530

    Add files via upload

---
+++ SQL/Activities/sqlscript.sql
@@ -0,0 +1,111 @@
+REM   Script: SQL Activity
+REM   SQL Activity
+
+select * from scott.emp;
+
+CREATE TABLE salesman;
+
+CREATE TABLE tablename;
+
+CREATE TABLE tablename;
+
+CREATE TABLE tablename salesman;
+
+REATE TABLE tableName;
+
+
+CREATE TABLE tableName(Salesman);
+
+CREATE TABLE tableName;
+
+CREATE TABLE tableName(Salesman);
+
+CREATE TABLE tableName(Salesman);
+
+CREATE TABLE tableName(Salesman);
+
+CREATE TABLE salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+SELECT * FROM salesman;
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+SELECT * FROM salesman;
+
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+SELECT * FROM salesman;
+
+select distinct salesman_id from orders;
+
+select distinct salesman_id from orders;
+
+select order_no, order_date from orders order by order_date;
+
+select order_no, purchase_amount from orders order by purchase_amount DESC;
+
+select * from orders where purchase_amount < 500;
+
+select * from orders where purchase_amount between 1000 and 2000;
+
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+SELECT * FROM salesman;
+
+SELECT * FROM Customers 
+ORDER BY Country;
+
+SELECT * FROM Customers 
+ORDER BY Country DESC;
+
+SELECT * FROM Customers 
+ORDER BY Country, CustomerName;
+
+SELECT * FROM Customers 
+ORDER BY Country ASC, CustomerName DESC;
+
